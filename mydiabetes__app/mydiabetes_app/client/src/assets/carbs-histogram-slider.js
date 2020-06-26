c196856b10478ea4d5be21b3ff0cb0140fc5f3ff
mr92furqan <mr92furqan@hotmail.com>
2020-06-26 17:17:58 +0500

    updated background color.

--- mydiabetes__app/mydiabetes_app/client/src/assets/carbs-histogram-slider.js
+++ mydiabetes__app/mydiabetes_app/client/src/assets/carbs-histogram-slider.js
@@ -1,7 +1,7 @@
 function CarbsSlider(histogram, legendColors, date, customOptions) {
 
 
-    let style = `<style> #carbs-slider svg { font-family: -apple-system, system-ui, "avenir next", avenir, helvetica, "helvetica neue", ubuntu, roboto, noto, "segoe ui", arial, sans-serif; } #carbs-slider rect.overlay { stroke: #888; } #carbs-slider rect.selection { stroke: none; fill: steelblue; fill-opacity: 0.4; } #labelleft, #labelright, #label-max, #label-min { font-size: 12px; } #carbs-slider #labelleft, #carbs-slider #labelright { dominant-baseline: hanging; } #carbs-slider #label-min, #carbs-slider #label-max { dominant-baseline: central; text-anchor: end; } </style>`;
+    let style = `<style> #carbs-slider svg { font-family: -apple-system, system-ui, "avenir next", avenir, helvetica, "helvetica neue", ubuntu, roboto, noto, "segoe ui", arial, sans-serif; } #carbs-slider rect.overlay { stroke: #888; } #carbs-slider rect.selection { stroke: none; fill: lightgray; fill-opacity: 0.4; } #labelleft, #labelright, #label-max, #label-min { font-size: 12px; } #carbs-slider #labelleft, #carbs-slider #labelright { dominant-baseline: hanging; } #carbs-slider #label-min, #carbs-slider #label-max { dominant-baseline: central; text-anchor: end; } </style>`;
 
     const defaultOptions = {
         'w':400,
@@ -76,7 +76,7 @@ function CarbsSlider(histogram, legendColors, date, customOptions) {
     hist1 = 0;
 
     groups.append('text')
-    .style('fill', 'white')
+    .style('fill', 'black')
     .attr('writing-mode', 'vertical-rl')
     .attr('font-size', 7)
     .attr('x', function(d) { let sum = x(d) + counter; counter+=11; return sum; })
